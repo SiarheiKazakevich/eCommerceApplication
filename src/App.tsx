5ceb7fa94044c7ecf5105a5d0875492960f275d1
Siarhei Kazakevich <dianrom@tut.by>
2026-02-24 18:44:28 +0300

    feat: add router and ways

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
 export default function App() {
-  return <h1>E-commerce App 🚀</h1>;
+  return (
+    <>
+      <nav style={{ display: "flex", gap: 16 }}>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
+  );
 }
